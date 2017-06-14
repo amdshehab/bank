ffad43789e1b28951d6bc704f4e375cd71f4a37c
Ahmed Shehab <shehab.ahmed.uk@gmail.com>
2017-06-14 18:08:23 +0100

    refactors code, completes bank tech test spec

--- spec/printerSpec.js
+++ spec/printerSpec.js
@@ -8,6 +8,14 @@ const transactionHistory = { '2017-6-14':
    [ { type: 'credit', transactionAmount: 1000, currentBalance: 1000 },
      { type: 'debit', transactionAmount: 500, currentBalance: 500 } ] }
 
+const multipleDateHistory = {
+  '2017-6-14':
+     [ { type: 'credit', transactionAmount: 1000, currentBalance: 1000 },
+       { type: 'debit', transactionAmount: 500, currentBalance: 500 } ],
+  '2017-6-13':
+  [ { type: 'debit', transactionAmount: 650, currentBalance: 700 } ]
+}
+
 describe('printer', function(){
   it('printer has a stanard message', function(){
     expect(printer.message).toEqual('date || credit || debit || balance \n')
@@ -15,6 +23,11 @@ describe('printer', function(){
   it('prints a statment for a specific date', function(){
     expect(printer.print(transactionHistory)).toEqual(
       'date || credit || debit || balance \n2017-6-14 || 1000 || || 1000 \n2017-6-14 || || 500 || 500 \n'
+    )
+  })
+  it('prints a statment for multiple dates', function(){
+    expect(printer.print(multipleDateHistory)).toEqual(
+      'date || credit || debit || balance \n2017-6-14 || 1000 || || 1000 \n2017-6-14 || || 500 || 500 \n2017-6-13 || || 650 || 700 \n'
     )
   })
 })
