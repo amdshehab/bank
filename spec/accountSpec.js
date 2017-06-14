5269473dc0724252b91bc71dbf44d5cceffb6737
Ahmed Shehab <shehab.ahmed.uk@gmail.com>
2017-06-14 12:08:12 +0100

    adds deposit function and date function

--- spec/accountSpec.js
+++ spec/accountSpec.js
@@ -1,9 +1,24 @@
 var Account = require('../lib/account')
 
+beforeEach(function(){
+  account = new Account
+})
 
 describe('balance', function(){
   it('is initiated with a balance of zero', function(){
-    var account = new Account
     expect(account.balance).toEqual(0)
+  })
+
+  it('transaction history is an emmpty object', function(){
+    expect(account.transactionHistory).toEqual({})
+  })
+
+  it('can deposit money into an account', function(){
+    account.deposit(200)
+    expect(account.balance).toEqual(200)
+  })
+
+  it('date returns current date', function(){
+    expect(account.date()).toEqual('2017-6-14')
   })
 })
